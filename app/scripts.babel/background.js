be754cd4dfe9518c8862bc4f6383ea720f0431ef
Dan Turco <dan.turco@aescentia.com>
2016-09-01 07:00:30 -0700

    add template files

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+  console.log('previousVersion', details);
 });
 
 chrome.browserAction.setBadgeText({text: '\Out'});
