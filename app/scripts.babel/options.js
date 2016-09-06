84d0e3d31e8cd79c145c51c1911bfb0dc5c27d22
Dan Turco <dan.turco@aescentia.com>
2016-09-05 18:19:33 -0700

    added options formatting and html

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,27 +1,33 @@
-console.log("\"Allo \"Allo! Option");
-
-
 const jpDefaultOptions = {
   transparency: 0.9,
   theme: "jp-theme-dark",
   autoOpenConsole: false
 };
 
+document.getElementById("transparency").addEventListener("change", function (event) {
+  document.getElementById("transparencyValue").innerText = event.target.value;
+});
+
 // Saves options to chrome.storage.sync.
 function saveOptions() {
   const transparency = document.getElementById("transparency").value;
   const theme = document.getElementById("theme");
+  const autoOpen = document.getElementById("autoOpen").checked;
   const options = {
-    transparency: transparency,
-    theme: theme.value
+    transparency: (transparency/100),
+    theme: theme.value,
+    autoOpenConsole: autoOpen
   };
+  console.log(options);
   chrome.storage.sync.set(options, () => {
     // Update status to let user know options were saved.
     const status = document.getElementById("status");
     status.textContent = "Options saved.";
+    status.style.display = "block";
     setTimeout(() => {
       status.textContent = "";
-    }, 750);
+      status.style.display = "none";
+    }, 2000);
   });
 
 }
@@ -30,13 +36,11 @@ function saveOptions() {
 // stored in chrome.storage.
 function restoreOptions() {
   chrome.storage.sync.get(jpDefaultOptions, (options) => {
-    document.getElementById("transparency").value = options.transparency;
+    document.getElementById("transparencyValue").innerText = document.getElementById("transparency").value = (options.transparency*100);
     document.getElementById("theme").value = options.theme;
-    console.log("options restored");
-    console.log(options);
+    document.getElementById("autoOpen").checked = options.autoOpenConsole;
   });
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("save").addEventListener("click",
-  saveOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
